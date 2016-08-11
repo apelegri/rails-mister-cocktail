13facd0c34a51394dc6976e6982400643c0cf5a3
adrien pelegri <pelegriadrien@gmail.com>
2016-08-11 12:08:10 +0200

    all models

---
+++ app/models/dose.rb
@@ -0,0 +1,12 @@
+class Dose < ApplicationRecord
+  belongs_to :cocktail
+  belongs_to :ingredient
+
+  validates :description, presence:true
+  validates :cocktail, presence:true
+  validates :ingredient, presence:true
+  validates :cocktail, uniqueness: { scope: :ingredient,
+    message: "cocktail ingredient couple should be unique" }
+
+
+end
